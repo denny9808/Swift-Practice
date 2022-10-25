d7fb929b04667b7a22bb3ad1e86af55e58c53a0e
denny9808 <116650768+denny9808@users.noreply.github.com>
2022-10-25 22:07:37 +0900

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  practice
+//
+//  Created by 박준호 on 2022/10/25.
+//
+
+import Foundation
+import SwiftUI
+
+var age: Int = 10
+
+print(age)
+
+print("hello, I am \(age+14)years old")
